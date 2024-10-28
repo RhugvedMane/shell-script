7ea927986ba2af7b606e93e71681b7271521ea71
root <root@DESKTOP-9VUUMTH>
2024-10-28 19:43:28 +0530

    file commited'

---
+++ shellscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Print a welcome message
+echo "Hello, welcome to my shell script!"
+
+# Print the current date and time
+echo "Current date and time: $(date)"
+
+# Print the current user
+echo "You are logged in as: $USER"
+
+# Print the current working directory
+echo "Your current directory is: $(pwd)"
+
+# List files in the current directory
+echo "Files in this directory:"
+ls -l
